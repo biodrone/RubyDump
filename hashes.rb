d8ad7580af5d1395636368b6d6e5351fccc35bd9
Josh Jacobs <josh.jacobs19@gmail.com>
2011-11-09 20:27:29 +0000

    Now you can search the hashes by key and it will return the value! I just need to know how to handle user input now...

--- hashes.rb
+++ hashes.rb
@@ -1,4 +1,12 @@
 #!/usr/bin/ruby
 
-x = { "a" => 1, "b" => 2 }
-x.each {
+x = { "a" => 1, "b" => 2, "c" => 3 } #adds keys and values
+#x.each { |key, value| puts "#{key} equals #{value}" } #iterates through the hashes and outputs in a readable format
+#puts x.keys #returns all keys
+#puts x.values #returns all values
+userkey = "a"
+x.each { |key, value|
+	if (userkey == key) then
+		puts "The value of that key is #{value}" 
+	end
+	}
